5214690da79da8564b090684bc99f346a4832763
Bjørnar Longva <longva85@hotmail.com>
2020-02-28 11:44:11 +0100

    Position can be set from console window

--- jsonTest/jsonTest.py
+++ jsonTest/jsonTest.py
@@ -2,21 +2,30 @@ import serial
 import time
 import json
 
-ser = serial.Serial('COM5', baudrate=19200, timeout=0.1)
+ser = serial.Serial('COM6', baudrate=19200, timeout=0.1)
 time.sleep(3)  # delay required before sending and receiving
+position = 0
 
 values = {
     "pos1": 0,
-    "pos2": 35,
-    "pos3": 70,
-    "pos4": 105,
-    "pos5": 140,
-    "pos6": 160,
+    "pos2": 0,
+    "pos3": 0,
+    "pos4": 0,
+    "pos5": 0,
+    "pos6": 0,
     "counter": 0,
 }
 
 
-def setValues():
+def setValues(position):
+    values["pos1"] = position
+    values["pos2"] = 180 - position
+    values["pos3"] = position
+    values["pos4"] = 180 - position
+    values["pos5"] = position
+    values["pos6"] = 180 - position
+
+
     ser.write(str.encode(json.dumps(values)))  # convert to json format, convert to bytes, write to serial port
     data = ser.readline()  # read input, convert to ascii string
     y = json.loads(data)  # convert string to python object
@@ -25,5 +34,6 @@ def setValues():
 
 
 while 1:
-    receivedValues = setValues()
+    val = input("Enter your value: ")
+    receivedValues = setValues(int(val))
     print(receivedValues)
